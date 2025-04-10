b6690fac9b37b364427dabf21fbc159a63f8dc45
Eyob Mengistu <emengistu17@gmail.com>
2025-04-10 11:25:24 -0500

    Added my shopping list

---
+++ Shopping-list/src/main/java/ShoppingList.java
@@ -0,0 +1,14 @@
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Banana");
+        System.out.println("Soap");
+        System.out.println("Bread");
+        System.out.println("Apple");
+        System.out.println("Juice");
+        System.out.println("Water");
+        System.out.println("Snack");
+        System.out.println("Onion");
+        System.out.println("Tomato");
+        System.out.println("Milk");
+    }
+}
