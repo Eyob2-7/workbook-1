42924012337cfdb6e51be6665c7560bc320602e9
Eyob Mengistu <emengistu17@gmail.com>
2025-04-15 09:25:06 -0500

    Update Completed Mod3 - Exercise 3 - Basic Calculator

--- BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -13,6 +13,7 @@ public class BasicCalculator {
         float num1 = myScanner.nextFloat();
         System.out.print("Enter the second number");
         float num2 = myScanner.nextFloat();
+        myScanner.nextLine();
 
         //Menu options
         System.out.println("\nPossible calculations:");
@@ -22,16 +23,61 @@ public class BasicCalculator {
         System.out.println("(D)ivide");
 
         //Ask for the user's options
-        System.out.print("Please select an option\n");
-        String option = myScanner.nextLine();
+        System.out.print("Please select an option:");
+        String option = myScanner.nextLine().toUpperCase();
 
-        //Always multiply regardless of option
-        float result = multiplication(num1,num2);
+        //Display user's option
+        System.out.println("The user chose " + option);
+
+        //Perform operation
+        float result = 0;
+        String operation = "";
+
+        if (option.equals("A")){
+            result = add(num1,num2);
+            operation ="+";
+        }
+        else if (option.equals("S")) {
+            result = subtract(num1, num2);
+            operation = "-";
+        } else if (option.equals("M")) {
+            result = multiply(num1, num2);
+            operation = "*";
+        } else if (option.equals("D")) {
+            if (num2 != 0) {
+                result = divide(num1, num2);
+                operation = "/";
+            } else {
+                System.out.println("Error: Cannot divide by zero.");
+                myScanner.close();
+                return;
+            }
+        } else {
+            System.out.println("Invalid option.");
+            myScanner.close();
+            return;
+        }
+
+        //output
+
         System.out.printf("%.2f * %.2f = %.2f%n" ,num1,num2,result);
         myScanner.close();
 
 
     }
-    public static float multiplication(float num1, float num2){
-        return num1 * num2;
+    //Static methods for each operation
+    public static float add(float a, float b) {
+        return a + b;
+    }
+
+    public static float subtract(float a, float b) {
+        return a - b;
+    }
+
+    public static float multiply(float a, float b) {
+        return a * b;
+    }
+
+    public static float divide(float a, float b) {
+        return a / b;
 }}
